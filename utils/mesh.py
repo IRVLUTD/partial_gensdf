efcd44eabf5e10e5c91ffeee4efa1e96cd3076ce
Jikai Wang <jikai.wang@utdallas.edu>
2023-05-01 12:20:18 -0500

    replace plyfile with open3d for ply file writing

--- utils/mesh.py
+++ utils/mesh.py
@@ -3,19 +3,26 @@
 import logging
 import math
 import numpy as np
-import plyfile
+
+# import plyfile
+import open3d as o3d
 import skimage.measure
 import time
 import torch
 
 
-# N: resolution of grid; 256 is typically sufficient 
+# N: resolution of grid; 256 is typically sufficient
 # max batch: as large as GPU memory will allow
 # shape_feature is either point cloud, mesh_idx (neuralpull), or generated latent code (deepsdf)
 def create_mesh(
-    model, shape_feature, filename, N=256, max_batch=1000000, level_set=0.0, occupancy=False
+    model,
+    shape_feature,
+    filename,
+    N=256,
+    max_batch=1000000,
+    level_set=0.0,
+    occupancy=False,
 ):
-    
     start_time = time.time()
     ply_filename = filename
 
@@ -29,41 +36,35 @@ def create_mesh(
 
     head = 0
     while head < cube_points:
-        
         query = cube[head : min(head + max_batch, cube_points), 0:3].unsqueeze(0)
-        
+
         # inference defined in forward function per pytorch lightning convention
         pred_sdf = model(shape_feature.cuda(), query)
 
         cube[head : min(head + max_batch, cube_points), 3] = pred_sdf
-            
+
         head += max_batch
-    
+
     # for occupancy instead of SDF, subtract 0.5 so the surface boundary becomes 0
-    sdf_values = cube[:, 3] - 0.5 if occupancy else cube[:, 3] 
+    sdf_values = cube[:, 3] - 0.5 if occupancy else cube[:, 3]
     sdf_values = sdf_values.reshape(N, N, N).detach().cpu()
 
-    #print("inference time: {}".format(time.time() - start_time))
+    # print("inference time: {}".format(time.time() - start_time))
 
     convert_sdf_samples_to_ply(
-        sdf_values.data,
-        voxel_origin,
-        voxel_size,
-        ply_filename + ".ply",
-        level_set
+        sdf_values.data, voxel_origin, voxel_size, ply_filename + ".ply", level_set
     )
 
 
 # create cube from (-1,-1,-1) to (1,1,1) and uniformly sample points for marching cube
 def create_cube(N):
-
-    overall_index = torch.arange(0, N ** 3, 1, out=torch.LongTensor())
-    samples = torch.zeros(N ** 3, 4)
+    overall_index = torch.arange(0, N**3, 1, out=torch.LongTensor())
+    samples = torch.zeros(N**3, 4)
 
     # the voxel_origin is the (bottom, left, down) corner, not the middle
     voxel_origin = [-1, -1, -1]
     voxel_size = 2.0 / (N - 1)
-    
+
     # transform first 3 columns
     # to be the x, y, z index
     samples[:, 2] = overall_index % N
@@ -81,13 +82,12 @@ def create_cube(N):
     return samples
 
 
-
 def convert_sdf_samples_to_ply(
     pytorch_3d_sdf_tensor,
     voxel_grid_origin,
     voxel_size,
     ply_filename_out,
-    level_set=0.0
+    level_set=0.0,
 ):
     """
     Convert sdf samples to .ply
@@ -102,7 +102,7 @@ def convert_sdf_samples_to_ply(
 
     numpy_3d_sdf_tensor = pytorch_3d_sdf_tensor.numpy()
 
-    # use marching_cubes_lewiner or marching_cubes depending on pytorch version 
+    # use marching_cubes_lewiner or marching_cubes depending on pytorch version
     try:
         verts, faces, normals, values = skimage.measure.marching_cubes(
             numpy_3d_sdf_tensor, level=level_set, spacing=[voxel_size] * 3
@@ -118,23 +118,26 @@ def convert_sdf_samples_to_ply(
     mesh_points[:, 1] = voxel_grid_origin[1] + verts[:, 1]
     mesh_points[:, 2] = voxel_grid_origin[2] + verts[:, 2]
 
-    num_verts = verts.shape[0]
-    num_faces = faces.shape[0]
+    # num_verts = verts.shape[0]
+    # num_faces = faces.shape[0]
 
-    verts_tuple = np.zeros((num_verts,), dtype=[("x", "f4"), ("y", "f4"), ("z", "f4")])
+    # verts_tuple = np.zeros((num_verts,), dtype=[("x", "f4"), ("y", "f4"), ("z", "f4")])
 
-    for i in range(0, num_verts):
-        verts_tuple[i] = tuple(mesh_points[i, :])
+    # for i in range(0, num_verts):
+    #     verts_tuple[i] = tuple(mesh_points[i, :])
 
-    faces_building = []
-    for i in range(0, num_faces):
-        faces_building.append(((faces[i, :].tolist(),)))
-    faces_tuple = np.array(faces_building, dtype=[("vertex_indices", "i4", (3,))])
+    # faces_building = []
+    # for i in range(0, num_faces):
+    #     faces_building.append(((faces[i, :].tolist(),)))
+    # faces_tuple = np.array(faces_building, dtype=[("vertex_indices", "i4", (3,))])
 
-    el_verts = plyfile.PlyElement.describe(verts_tuple, "vertex")
-    el_faces = plyfile.PlyElement.describe(faces_tuple, "face")
+    # el_verts = plyfile.PlyElement.describe(verts_tuple, "vertex")
+    # el_faces = plyfile.PlyElement.describe(faces_tuple, "face")
 
-    ply_data = plyfile.PlyData([el_verts, el_faces])
-    ply_data.write(ply_filename_out)
-
-
+    # ply_data = plyfile.PlyData([el_verts, el_faces])
+    # ply_data.write(ply_filename_out)
+    verts_o3d = o3d.utility.Vector3dVector(mesh_points)
+    faces_o3d = o3d.utility.Vector3iVector(faces)
+    mesh_o3d = o3d.geometry.TriangleMesh(verts_o3d, faces_o3d)
+    mesh_o3d.compute_vertex_normals()
+    o3d.io.write_triangle_mesh(ply_filename_out, mesh_o3d)
